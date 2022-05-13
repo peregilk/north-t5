dd8e5ef4a3a63e3bf7795ee81ff9d563a8329efb
Per Egil Kummervold <per@capia.no>
2022-05-13 11:18:52 +0000

    path change

--- utils/run_xxl.sh
+++ utils/run_xxl.sh
@@ -1,5 +1,5 @@
 T5X_CHECKPOINT_PATH="gs://north-t5x/pretrained_models/xxl/norwegian_NCC_plus_English_t5x_xxl/checkpoint_1500000"
-FLAX_DUMP_FOLDER_PATH="/home/perk/conv/north-t5-xxl/"
+FLAX_DUMP_FOLDER_PATH="/home/perk/models/north-t5-xxl/"
 CONFIG_NAME="xxl.json"
 
 python convert_t5x_checkpoint_to_flax.py --t5x_checkpoint_path ${T5X_CHECKPOINT_PATH} --config_name ${CONFIG_NAME} --flax_dump_folder_path ${FLAX_DUMP_FOLDER_PATH}
