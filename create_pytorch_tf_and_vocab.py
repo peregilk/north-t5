2adb2710e4a08ab51e265fb2f6c5d3ecf63d9eb7
Per Egil Kummervold <per@capia.no>
2022-05-21 13:32:47 +0000

    lots of changes

---
+++ create_pytorch_tf_and_vocab.py
@@ -0,0 +1,41 @@
+from transformers import TFT5ForConditionalGeneration, T5ForConditionalGeneration, T5Config, AutoTokenizer
+import shutil
+import argparse
+
+
+def create_pytorch_and_vocab(flax_dump_folder_path, model_size):
+
+    shutil.copyfile('spiece.model',flax_dump_folder_path+'/spiece.model')
+    shutil.copyfile('special_tokens_map.json',flax_dump_folder_path+'/special_tokens_map.json')
+    shutil.copyfile('tokenizer_config.json',flax_dump_folder_path+'/tokenizer_config.json')
+    shutil.copyfile('.gitattributes',flax_dump_folder_path+'/.gitattributes')
+
+    print("Copied tokenizer files and a gitattributes file")
+    config= T5Config.from_pretrained(model_size+'.json')
+    model = T5ForConditionalGeneration(config=config)
+    model.save_pretrained(flax_dump_folder_path)
+
+    print("Saved PyTorch-model")
+
+    model = TFT5ForConditionalGeneration(config=config)
+    model.save_pretrained(flax_dump_folder_path)
+
+    print("Saved Tensorflow-model")
+
+    tokenizer = AutoTokenizer.from_pretrained(flax_dump_folder_path)
+    tokenizer.save_pretrained(flax_dump_folder_path)
+
+    print("Saved tokenizer")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument(
+        "--flax_dump_folder_path", default=None, type=str, required=True, help="Path to the output FLAX model."
+    )
+    parser.add_argument(
+        "--model_size", default="base", type=str, required=True, help="Model size. Used for finding the correct config file."
+    )
+    args = parser.parse_args()
+    create_pytorch_and_vocab(args.flax_dump_folder_path)
--- t5paths.py
+++ t5paths.py
@@ -16,44 +16,44 @@ def t5paths(size="all"):
 
             n = "mt5_"+s+"_NCC"
             p = "gs://north-t5x/pretrained_models/"+s+"/norwegian_NCC_plus_English_byt5x_"+s+"/"
-            c = p+"checkpoint_15000000"
+            c = p+"checkpoint_1500000"
             if s!="xl" and s!="xxl":
-                paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+                paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
             
             n = "t5_"+s+"_scand"
             p = "gs://north-t5x/pretrained_models/"+s+"/scandinavian_t5x_"+s+"/"
-            c = p+"checkpoint_15000000"
+            c = p+"checkpoint_1700000"
             if s!="xl" and s!="xxl":
-                paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+                paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
             
             n = "t5_"+s+"_NCC_lm"
             p = "gs://north-t5x/pretrained_models/"+s+"/norwegian_NCC_plus_English_pluss100k_lm_t5x_"+s+"/"
-            c = p+"checkpoint_16000000"
-            paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+            c = p+"checkpoint_1600000"
+            paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
             
             n = "t5_"+s+"_NCC_modern_lm"
             p = "gs://north-t5x/pretrained_models/"+s+"/norwegian_NCC_plus_English_pluss200k_balanced_bokmaal_nynorsk_pluss100k_lm_t5x_"+s+"/"
-            c = p+"checkpoint_18000000"
+            c = p+"checkpoint_1800000"
             if s!="xxl":
-                paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+                paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
             
             n = "t5_"+s+"_NCC_modern"
             p = "gs://north-t5x/pretrained_models/"+s+"/norwegian_NCC_plus_English_pluss200k_balanced_bokmaal_nynorsk_t5x_"+s+"/"
-            c = p+"checkpoint_16000000"
+            c = p+"checkpoint_1600000"
             if s!="xxl":
-                paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+                paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
             
             n = "t5_"+s+"_NCC_scand"
             p = "gs://north-t5x/pretrained_models/"+s+"/norwegian_NCC_plus_English_pluss200k_scandinavian_t5x_"+s+"/"
-            c = p+"checkpoint_17000000"
+            c = p+"checkpoint_1700000"
             if s!="xxl":
-                paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+                paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
             
             private = False
             n = "t5_"+s+"_NCC"
             p = "gs://north-t5x/pretrained_models/"+s+"/norwegian_NCC_plus_English_t5x_"+s+"/"
-            c = p+"checkpoint_15000000"
-            paths.append({"name":n,"path":p,"checkpoint":c,"private":private})
+            c = p+"checkpoint_1500000"
+            paths.append({"name":n,"path":p,"checkpoint":c,"private":private,"size":s})
 
     return paths
 
